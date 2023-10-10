7779a85088d52b85f67659ce899b2f2a9aa8b1b3
Muhsubhan04 <147331538+Muhsubhan04@users.noreply.github.com>
2023-10-10 16:39:06 +0800

    day 2
    hari ke dua ini saya membuat program yg menampilkann variabel

---
+++ var.java
@@ -0,0 +1,16 @@
+public class variabel {
+    public static void main(String[] args) {
+        // Variabel umum
+        String nama = "Subhan";
+        int usia = 19;
+        double tinggi = 160.3;
+        char jenisKelamin = 'L'; // 'L' untuk laki-laki
+        boolean belumMenikah = true;
+
+        // Menampilkan nilai variabel ke layar
+        System.out.println("Profil Subhan:");
+        System.out.println("Nama: " + nama);
+        System.out.println("Usia: " + usia + " tahun");
+        System.out.println("Tinggi: " + tinggi + " cm");
+    }
+}
